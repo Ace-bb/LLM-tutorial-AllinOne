346bb3ebbde7f16d2749e589ed1d6155633f01d0
Ace-bb <13489323285@163.com>
2024-12-13 22:43:36 +0800

    DPO

--- trans2md.py
+++ trans2md.py
@@ -1,8 +1,9 @@
 import json, re, requests
 from urllib import parse
 from tqdm import tqdm
+import shutil, os
 
-IMAGE_CLOUD_URL="https://gitee.com/Ace_bb/transformer/raw/main"
+IMAGE_CLOUD_URL="https://gitee.com/Ace_bb/static_resource_cloud/raw/master"
 
 QA_ID = 0
 # 发送Http给Get请求
@@ -22,12 +23,16 @@ def upload_image_to_weixin(image_path, access_token):
     response = requests.post(url, files=files)
     return response.json()
 
-def trans_cell_one_img(img_row):
+def trans_cell_one_img(img_row, folder_path):
     img_src = re.search(r'src="(.*?)"', img_row).group(1)
+    folder_path,img_src = folder_path.replace('./', ''), img_src.replace('./', '')
+    if not os.path.exists(f"repo/static_resource_cloud/{folder_path}/_img"): os.makedirs(f"repo/static_resource_cloud/{folder_path}/_img")
+    shutil.copy(f"{folder_path}/{img_src}", f"repo/static_resource_cloud/{folder_path}/{img_src}")
+    img_src = f"{folder_path}/{img_src}"
     img_url = f"{IMAGE_CLOUD_URL}/{img_src}"
     return img_row.replace(img_src, img_url)
 
-def trans_cell_multi_img(multi_img_content):
+def trans_cell_multi_img(multi_img_content, folder_path):
     '''<p align="center">
         <img src="imgs/position/position_encoding_ex.png" width="50%">
     </p>'''
@@ -35,9 +40,13 @@ def trans_cell_multi_img(multi_img_content):
     img_rows = multi_img_content
     for rid, row in enumerate(img_rows):
         if row.strip().startswith('<img'):
-            img_src = re.search(r'src="(.*?)"', row).group(1)
+            ori_img_src = re.search(r'src="(.*?)"', row).group(1)
+            folder_path,img_src = folder_path.replace('./', ''), ori_img_src.replace('./', '')
+            if not os.path.exists(f"repo/static_resource_cloud/{folder_path}/_img"): os.makedirs(f"repo/static_resource_cloud/{folder_path}/_img")
+            shutil.copy(f"{folder_path}/{img_src}", f"repo/static_resource_cloud/{folder_path}/{img_src}")
+            img_src = f"{folder_path}/{img_src}"
             img_url = f"{IMAGE_CLOUD_URL}/{img_src}"
-            img_rows[rid] = row.replace(img_src, img_url).strip(" ")
+            img_rows[rid] = row.replace(ori_img_src, img_url).strip(" ")
     # print('\n'.join(img_rows))
     return '\n'.join(img_rows) + '\n'
 
@@ -46,15 +55,18 @@ def trans_code_cell_2(code_source):
     cell_codes = []
     return f"```python\n{''.join(code_source)}\n```"
 
-def read_cells(notebook):
+def parse_notebook_content(folder_path, notebook_name):
+    notebook_path = f"{folder_path}/{notebook_name}"
+    notebook = read_notebook(notebook_path)
     cells = notebook['cells']
     artical_content =  []
     H1_Title_Id= 0
-    for i, cell in tqdm(enumerate(cells), total=len(cells)):
+    for i, cell in tqdm(enumerate(cells), total=len(cells), desc=notebook_path):
         if cell['cell_type'] == 'markdown':
             for rid, row in enumerate(cell['source']):
                 if row.strip().startswith('<img'):
-                    artical_content.append(trans_cell_one_img(row)) # 单张图片
+                    one_img_Content = trans_cell_one_img(row, folder_path)
+                    artical_content.append(one_img_Content) # 单张图片
                 elif row.strip().startswith('<p'):
                     multi_img_content = []
                     for j in range(rid, len(cell['source'])):
@@ -62,25 +74,40 @@ def read_cells(notebook):
                             multi_img_content = cell['source'][rid:j+1]
                             cell['source'][rid:j+1] = ['']*(j-rid+1)
                             break
-                    artical_content.append(trans_cell_multi_img(multi_img_content)) # 多张图片
+                    mic = trans_cell_multi_img(multi_img_content, folder_path)
+                    artical_content.append(mic) # 多张图片
                 else:
                     artical_content.append(row)
         elif cell['cell_type'] == 'code':
-            artical_content.append(trans_code_cell_2(cell['source']))
+            artical_content.append("\n\n" + trans_code_cell_2(cell['source']) + "\n\n")
     
     return artical_content
 
 def parse_notebook(note_book_path):
-    notebook = read_notebook(note_book_path)
-    artical_content = read_cells(notebook)
-    for _id, item in enumerate(artical_content):
-        if item==None:
-            print(_id)
-            artical_content[_id] = '\n'
-            
-    with open(note_book_path.replace('.ipynb', '.md'), 'w', encoding='utf-8') as f:
-        f.write(''.join(artical_content))
-        
+    for l_1_folder in os.listdir("./"):
+        if l_1_folder==".git": continue
+        if not os.path.isdir(f"./{l_1_folder}"): continue
+        for l_2_folder in os.listdir(f"./{l_1_folder}"):
+            if l_2_folder=="1.2-Transformer": continue
+            if not os.path.isdir(f"./{l_1_folder}/{l_2_folder}"): continue
+            for f_name in os.listdir(f"./{l_1_folder}/{l_2_folder}"):
+                # if f_name !="LLaMA.ipynb": continue
+                try:
+                    if not f_name.endswith(".ipynb"): continue
+                    # notebook_path = f"./{l_1_folder}/{l_2_folder}/{f_name}"
+                    artical_content = parse_notebook_content(f"./{l_1_folder}/{l_2_folder}", f_name)
+                    for _id, item in enumerate(artical_content):
+                        if item==None:
+                            print(_id)
+                            artical_content[_id] = '\n'
+                    res_save_path = "repo/parse_notebooks/" + note_book_path.replace('.ipynb', '.md')
+                    base_dir = os.path.dirname(res_save_path)
+                    if not os.path.exists(base_dir): os.makedirs(base_dir)
+                    with open(res_save_path, 'w', encoding='utf-8') as f:
+                        f.write(''.join(artical_content))
+                except:
+                    continue
+
 if __name__ == '__main__':
     parse_notebook('2-主流模型架构/2.2-LLaMA/LLaMA.ipynb')
     # access_token = get_weixin_access_token()['access_token'] #"85_5jStf6LK3xA73EyKauZR72t4fjpcD_Bkv96N2Y3u8Xz0iN7UnEA7DY0y9oBBlF5y3lCuVoLvgSr_WUKuht_tiDHZV3vBK9TtGmf2kneCcw6sRp-as9GzEDBJ1PEZTLaAJATNF" # get_weixin_access_token()['access_token']
